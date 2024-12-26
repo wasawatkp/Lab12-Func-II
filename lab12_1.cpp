f4ab9e3a96055bce53f40c31cfbac9343d6d8f20
wasawatkp <wasawat.upper@gmail.com>
2024-12-27 01:00:26 +0700

    lab12_1 Clear

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(unsigned int x);
+
 int main(){
     int x;
     cin >> x;
-    cout << fibonacci(x);
+    cout << fibonacci(15);
     return 0; 
+}
+
+int fibonacci(unsigned int x){
+    if (x == 0) return 0;
+    else if (x == 1) return  1;
+    else  return  fibonacci(x-1) + fibonacci(x-2);
 }
