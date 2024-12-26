e709ec6b43cabf3148429b3221add661c9a6bea1
wasawatkp <wasawat.upper@gmail.com>
2024-12-27 01:07:29 +0700

    lab12_1 Clear

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,16 +1,19 @@
 #include<iostream>
+#include <ctime>
 using namespace std;
 
-int fibonacci(unsigned int x);
+long long int fibonacci(int x);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(15);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
-int fibonacci(unsigned int x){
+long long int fibonacci(int x){
     if (x == 0) return 0;
     else if (x == 1) return  1;
     else  return  fibonacci(x-1) + fibonacci(x-2);
